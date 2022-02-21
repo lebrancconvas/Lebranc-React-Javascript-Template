3656590c144946766bb2d39b7d9036271332355f
lebrancconvas <lebrancconvas@gmail.com>
2022-02-22 06:51:42 +0700

    💄 Create Homepage UI.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Homepage from './pages/Homepage';
 function App() {
   return (
     <div>
-      
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+    return ( 
+      <div>
+				<h1>Hello World.</h1>
+			</div>
+    )
+}
+
+export default Homepage;
